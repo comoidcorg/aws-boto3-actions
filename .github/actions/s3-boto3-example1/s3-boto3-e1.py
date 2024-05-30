6bf4696d52592fa137eaf733fa1d7c22c1c4435e
Murali D <muralindiablog@gmail.com>
2024-05-29 17:07:59 -0700

    output test

--- .github/actions/s3-boto3-example1/s3-boto3-e1.py
+++ .github/actions/s3-boto3-example1/s3-boto3-e1.py
@@ -14,21 +14,22 @@ def create_bucket():
             s3_client = boto3.client('s3')
             s3_client.create_bucket(Bucket=bucket_name)
         # elif region == "us-east-1":
-        #     s3_client = boto3.client('s3')
-        #     s3_client.create_bucket(Bucket=bucket_name)
+        #      s3_client = boto3.client('s3')
+        #      s3_client.create_bucket(Bucket=bucket_name)
         else:
             s3_client = boto3.client('s3', region_name=region)
             location = {'LocationConstraint': region}
             s3_client.create_bucket(Bucket=bucket_name,
                                     CreateBucketConfiguration=location)
-            
-        # with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
-        #    print(f'Bucketn={bucket_name}', file=gh_output)
+        
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+            print(f'BucketnRegion={bucket_name} {region}', file=gh_output)
 
     except ClientError as e:
         logging.error(e)
         return False
-    return True
+    return True    
+    
 
 if __name__ == '__main__':
     create_bucket()
